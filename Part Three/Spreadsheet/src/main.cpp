5e30f6301000e2ed698e18e32ef739c394287389
Kimowich <kimkofnie@gmail.com>
2022-01-03 16:50:47 +0100

    Added test code to main

--- Part Three/Spreadsheet/src/main.cpp
+++ Part Three/Spreadsheet/src/main.cpp
@@ -1,14 +1,29 @@
-#include <iostream>
 #include <SpreadSheet.h>
-#include <memory>
-
+#include <vector>
 void printSpreadSheet(SpreadSheet sheet){
     // This function deletes the data that s1 is pointing too
 ;
 }
 
+SpreadSheet createObject()
+{
+    return SpreadSheet{ 3,2 };
+}
+
 int main(int, char**) {
-    SpreadSheet s1{4,3};
+    std::vector<SpreadSheet> vec;
+    for (size_t i = 0; i < 2; i++)
+    {
+        std::cout << "Iteration: " << i << std::endl;
+        vec.push_back(SpreadSheet{ 100,100 });
+        std::cout << std::endl;
+    }
+
+    SpreadSheet s1{ 2,3 };
+    s1 = createObject();
+
+    SpreadSheet s2{ 5,6 };
+    s2 = s1;
 
     return EXIT_SUCCESS;
 }
